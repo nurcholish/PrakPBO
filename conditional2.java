a1f19b4ba7b0af48f4449c60316bcf72b881a07f
nurcholish <60200112010@uin-alauddin.ac.id>
2015-06-28 16:13:14 +0800

    laporan4

---
+++ conditional2.java
@@ -0,0 +1,19 @@
+package cholis;
+
+public class conditional2 {
+	
+	public static void main (String[] args) {
+		int nilai1 = 1;
+		int nilai2 = 2;
+		int hasil;
+		boolean kondisi = true;
+		hasil = kondisi ? nilai1 : nilai2;
+		if (kondisi == true)
+			hasil = nilai1;
+		else
+			hasil = nilai2;
+		
+		System.out.println (hasil);
+	}
+
+}
